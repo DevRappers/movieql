23dcc483547ec37520cb137cb496561ff15a7b1d
DevRappers <devrappers@gmail.com>
2019-11-14 15:23:42 +0900

    외부 API에서 데이터 불러오기

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,17 +1,16 @@
-import { getMovies, getById, addMovie, deleteMovie} from "./db";
+import { getMovies } from "./db";
 
 // Resolvers는 Query를 해결하는 것으로 
 // Query는 Database에게는 문제같은 것이고 이것을 어떤 방식으로 resolve(해결) 해야함.
 const resolvers = {
     // 사용자가 쿼리로 name을 호출하면 DevRappers를 반환함.
     Query: {
-        movies: () => getMovies(),
-        movie: (_, { id }) =>  getById(id)
-    },
-    Mutation: {
-        addMovie: (_, {name, score}) => addMovie(name, score),
-        deleteMovie: (_,{id}) => deleteMovie(id)
+        movies: () => getMovies()
     }
+    // Mutation: {
+    //     addMovie: (_, {name, score}) => addMovie(name, score),
+    //     deleteMovie: (_,{id}) => deleteMovie(id)
+    // }
 }
 
 export default resolvers;
