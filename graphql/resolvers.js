93e5c707d52742f3f35ace2d7c080cb108f42511
DevRappers <devrappers@gmail.com>
2019-11-14 14:11:30 +0900

    type을 지정해서 불러오기

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,16 @@
 // Resolvers는 Query를 해결하는 것으로 
 // Query는 Database에게는 문제같은 것이고 이것을 어떤 방식으로 resolve(해결) 해야함.
+
+const devrappers = {
+    name: 'JJL',
+    age: 22,
+    gender: 'male'
+}
+
 const resolvers = {
     // 사용자가 쿼리로 name을 호출하면 DevRappers를 반환함.
     Query: {
-        name: () => "DevRappers"
+        person: () => devrappers
     }
 }
 
